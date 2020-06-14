9c6c21ceafe11bff12f489d50c984f3bdd3afd94
Abhishek Joshi <abhishek.joshi@nagarro.com>
2020-06-14 15:56:12 +0530

    fix issue

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-// const path = require('path');
+const path = require('path');
 
 app.use(express.static(__dirname + '/build'));
 
-// app.get('*', (request, response) => {
-//    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-// });
+app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build/index.html'), function (err) {
+      if (err) {
+         res.status(500).send(err)
+      }
+   })
+})
 
 app.listen(port, () => {
    console.log('Server is up on', port);
